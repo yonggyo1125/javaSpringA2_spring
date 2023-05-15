5a7913b8661ef2c5c9f704ca534029b8414c0451
yonggyo1125 <yonggyo00@kakao.com>
2023-05-15 15:51:54 +0900

    강의소스

--- day04/src/main/java/main/Ex01.java
+++ day04/src/main/java/main/Ex01.java
@@ -8,8 +8,8 @@ public class Ex01 {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
         EmpDao empDao = ctx.getBean(EmpDao.class);
-        empDao.insert();
-
+        //empDao.insert();
+        empDao.delete(2);
         ctx.close();
     }
 }
--- day04/src/main/java/models/emp/EmpDao.java
+++ day04/src/main/java/models/emp/EmpDao.java
@@ -10,7 +10,13 @@ public class EmpDao {
 
     public void insert() {
         String sql = "INSERT INTO EMP2 (EMPNO, ENAME, JOB) VALUES (?, ?, ?)";
-        int affectedRows = jdbcTemplate.update(sql, 1, "사원1", "STAFF");
+        int affectedRows = jdbcTemplate.update(sql, 2, "사원2", "STAFF");
+        System.out.println(affectedRows);
+    }
+
+    public void delete(int no) {
+        String sql = "DELETE FROM EMP2 WHERE EMPNO = ?";
+        int affectedRows = jdbcTemplate.update(sql, no);
         System.out.println(affectedRows);
     }
 }
