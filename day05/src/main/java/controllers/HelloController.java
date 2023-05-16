579285ed9485bbc63051ae8898ba53801112ef19
yonggyo1125 <yonggyo00@kakao.com>
2023-05-16 15:53:12 +0900

    수업 소스

--- day05/src/main/java/controllers/HelloController.java
+++ day05/src/main/java/controllers/HelloController.java
@@ -2,11 +2,33 @@ package controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
+
+    /**
+    public ModelAndView hello() {
+
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("hello");
+        return mv;
+    }
+     */
+
+    /**
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(String name, int num1, int num2, boolean agree) {
+        System.out.println(name);
+        System.out.println(num1 + num2);
+        System.out.println(agree);
+        return "hello";
+    }
+    */
+    @GetMapping("/hello")
+    public String hello(@RequestParam("nm") String name) {
+        System.out.println(name);
 
         return "hello";
     }
