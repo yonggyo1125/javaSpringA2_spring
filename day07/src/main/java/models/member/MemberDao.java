42a0f278027228d452c4ff1d1844ca57dfa60b73
yonggyo1125 <yonggyo00@kakao.com>
2023-05-19 15:29:10 +0900

    수업 소스

--- day07/src/main/java/models/member/MemberDao.java
+++ day07/src/main/java/models/member/MemberDao.java
@@ -3,7 +3,11 @@ package models.member;
 import controllers.JoinForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Repository
 @RequiredArgsConstructor
@@ -11,17 +15,44 @@ public class MemberDao {
 
     private final JdbcTemplate jdbcTemplate;
 
-    public void insert(JoinForm joinForm) {
+    public boolean insert(JoinForm joinForm) {
+        String sql = "INSERT INTO MEMBER (USERNO, USERID, USERPW, USERNM) " +
+                    "VALUES (MEMBER_SEQ.nextval, ?, ?, ?)";
 
+        int cnt = jdbcTemplate.update(sql,
+                    joinForm.getUserId(), joinForm.getUserPw(), joinForm.getUserNm());
+
+        return cnt > 0;
     }
 
     public Member get(String userId) {
+        try {
+            String sql = "SELECT * FROM MEMBER WHERE USERID = ?";
+            Member member = jdbcTemplate.queryForObject(sql, this::mapper, userId);
 
-        return null;
+            return member;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public boolean exists(String userId) {
 
-        return false;
+        String sql = "SELECT COUNT(*) FROM MEMBER WHERE USERID = ?";
+        long cnt = jdbcTemplate.queryForObject(sql, Long.class);
+
+        return cnt > 0;
+    }
+
+    private Member mapper(ResultSet rs, int i) throws SQLException {
+        Member member = new Member();
+        member.setUserNo(rs.getLong("USERNO"));
+        member.setUserId(rs.getString("USERID"));
+        member.setUserPw(rs.getString("USERPW"));
+        member.setUserNm(rs.getString("USERNM"));
+        member.setRegDt(rs.getTimestamp("REGDT").toLocalDateTime());
+
+        return member;
     }
 }
